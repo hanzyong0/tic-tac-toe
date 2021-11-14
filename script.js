6c75d7d6ed226381ade5425cf00b524c495103a3
Zhi Yong Han <hanzyong0@gmail.com>
2021-11-14 21:14:18 +0800

    add textContent to display

--- script.js
+++ script.js
@@ -57,33 +57,44 @@ const Game = (() => {
 
     const checkWinner = (() => {
         const { board } = GameBoard;
+        const display = document.querySelector('.display');
         if (board[0] !== '' && board[0] === board[1] && board[1] === board[2]) {
+            display.textContent = `${winningPlayer} Wins!`
             removeClick();
         };
         if (board[3] !== '' && board[3] === board[4] && board[4] === board[5]) {
+            display.textContent = `${winningPlayer} Wins!`
             removeClick();
         };
         if (board[6] !== '' && board[6] === board[7] && board[7] === board[8]) {
+            display.textContent = `${winningPlayer} Wins!`
             removeClick();
         };
         if (board[0] !== '' && board[0] === board[3] && board[3] === board[6]) {
+            display.textContent = `${winningPlayer} Wins!`
             removeClick();
         };
         if (board[1] !== '' && board[1] === board[4] && board[4] === board[7]) {
+            display.textContent = `${winningPlayer} Wins!`
             removeClick();
         };
         if (board[2] !== '' && board[2] === board[5] && board[5] === board[8]) {
+            display.textContent = `${winningPlayer} Wins!`
+            display.textContent = `${winningPlayer} Wins!`
             removeClick();
         };
         if (board[0] !== '' && board[0] === board[4] && board[4] === board[8]) {
+            display.textContent = `${winningPlayer} Wins!`
             removeClick();
         };
         if (board[2] !== '' && board[2] === board[4] && board[4] === board[6]) {
+            display.textContent = `${winningPlayer} Wins!`
             removeClick();
         };
         if (board[0] !== '' && board[1] !== '' && board[2] !== '' && board[3] !== ''
             && board[4] !== '' && board[5] !== '' && board[6] !== '' && board[7] !== '' &&
             board[8] !== '') {
+            display.textContent = `Draw!`
             removeClick();
         };
     });
@@ -91,5 +102,11 @@ const Game = (() => {
 })();
 
 // Reset module IIFE
+// const reset = (() => {
+//     const resetBtn = document.querySelector('.reset');
 
+//     resetBtn.addEventListener('click', () => {
+
+//     })
+// })();
 
