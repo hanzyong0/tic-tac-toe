ca85efe206a3212159c32cf37101f65e782d7520
Zhi Yong Han <hanzyong0@gmail.com>
2021-11-14 20:32:41 +0800

    add GameBoard and Game modules

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+// GameBoard module IIFE
+const GameBoard = (() => {
+    const board = ['', '', '', '', '', '', '', '', ''];
+    return { board };
+})();
 
+// Factory function to create multiple players
+const Player = (name, symbol) => {
+    return { name, symbol };
+};
+
+// Create players with Player factory function
+const player1 = Player('Player 1', 'X');
+const player2 = Player('Player 2', 'O');
+
+// Game module IIFE
+const Game = (() => {
+    const { board } = GameBoard;
+
+    let winningPlayer = '';
+    let symbol = '';
+
+    const playerSelection = (e) => {
+        const index = e.target.id;
+        if (symbol === '' && board[index] === '') {
+            winningPlayer = player1.name;
+            symbol = player1.symbol;
+            board[index] = player1.symbol;
+            e.target.textContent = player1.symbol;
+        } else if (symbol === player1.symbol && board[index] === '') {
+            winningPlayer = player2.name;
+            symbol = player2.symbol;
+            board[index] = player2.symbol;
+            e.target.textContent = player2.symbol;
+        } else if (symbol === player2.symbol && board[index] === '') {
+            winningPlayer = player1.name;
+            symbol = player1.symbol;
+            board[index] = player1.symbol;
+            e.target.textContent = player1.symbol;
+        };
+        console.log(symbol);
+        console.log(board);
+        checkWinner();
+    };
+
+    const cells = document.querySelectorAll('.cell');
+
+    cells.forEach((cell) => {
+        cell.addEventListener('click', playerSelection);
+    });
+
+    const removeClick = () => {
+        cells.forEach((cell) => {
+            cell.removeEventListener('click', playerSelection);
+        });
+    };
+
+    const checkWinner = (() => {
+        const { board } = GameBoard;
+        if (board[0] !== '' && board[0] === board[1] && board[1] === board[2]) {
+            removeClick();
+        };
+        if (board[3] !== '' && board[3] === board[4] && board[4] === board[5]) {
+            removeClick();
+        };
+        if (board[6] !== '' && board[6] === board[7] && board[7] === board[8]) {
+            removeClick();
+        };
+        if (board[0] !== '' && board[0] === board[3] && board[3] === board[6]) {
+            removeClick();
+        };
+        if (board[1] !== '' && board[1] === board[4] && board[4] === board[7]) {
+            removeClick();
+        };
+        if (board[2] !== '' && board[2] === board[5] && board[5] === board[8]) {
+            removeClick();
+        };
+        if (board[0] !== '' && board[0] === board[4] && board[4] === board[8]) {
+            removeClick();
+        };
+        if (board[2] !== '' && board[2] === board[4] && board[4] === board[6]) {
+            removeClick();
+        };
+        if (board[0] !== '' && board[1] !== '' && board[2] !== '' && board[3] !== ''
+            && board[4] !== '' && board[5] !== '' && board[6] !== '' && board[7] !== '' &&
+            board[8] !== '') {
+            removeClick();
+        };
+    });
+    return {};
+})();
+
+// Reset module IIFE
 
 
